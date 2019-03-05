dd8ef6533293b6f08d2450071e658d2ac521b5b3
Kristi Bischoff <kristi.bischoff7@gmail.com>
2019-03-04 21:31:20 -0700

    updating app code

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+from bson import Binary, Code
+from bson.json_util import dumps
+#import json
 import craftapp
+
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/craftapp")
 
 
 # Route to render index.html template using data from Mongo
@@ -14,10 +18,12 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    beer_data = mongo.db.collection.find_one()
+    beer_data = dumps(mongo.db.collection.find())
+    return(beer_data)
 
     # Return template and data
-    return render_template("index.html", craftapp=beer_data)
+    return render_template("index.html", craft_info=beer_data)
+
 
 
 # Route that will trigger the scrape function
@@ -26,12 +32,15 @@ def scrape():
 
     # Run the scrape function
     beer_data = craftapp.scrape_info()
+    print(beer_data)
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, beer_data, upsert=True)
+    mongo.db.collection.drop()
+    mongo.db.collection.insert_many(beer_data)
 
     # Redirect back to home page
     return redirect("/")
+
 
 
 if __name__ == "__main__":
--- craftapp.py
+++ craftapp.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 def init_browser():
-    browser = Browser("chrome", headless=False)
+    return Browser("chrome", headless=False)
 
 # File to Load- obtain brewery csv
 file_to_load2 = "breweries.csv"
@@ -18,27 +18,34 @@ brewery_pd.head()
 #Drop unnecessary columns. We want the brewery name
 brewery1_df = brewery_pd.drop(columns=['Unnamed: 0'])
 brewery1_df['rating'] = ''
+brewery1_df.reset_index()
 
 def scrape_info():
-
+    browser = init_browser()
+    #craft_info = {}
 #Create a random sample of 20 breweries
-brewery2_df = brewery1_df.sample(20, random_state=0).copy()
+    brewery2_df = brewery1_df.sample(20, random_state=0).copy()
 
-base_url = 'https://untappd.com/'
+    base_url = 'https://untappd.com/'
 
 
-for index, record in brewery2_df.iterrows():
-    try:
-        brewery = record['name']
-        search_url = base_url + f'search?q={brewery}&type=brewery&sort='
-        browser.visit(search_url)
-        html = browser.html
-        soup = BeautifulSoup(html, 'html.parser')
-        rating = soup.find('span', class_="num").get_text()
-        brewery2_df.loc[index, 'rating'] = rating.strip("(").strip(")")
-    except (AttributeError):
-        print('No Values')
-    
-print(brewery2_df)
+    for index, record in brewery2_df.iterrows():
+        try:
+            brewery = record['name']
+            search_url = base_url + f'search?q={brewery}&type=brewery&sort='
+            browser.visit(search_url)
+            html = browser.html
+            soup = BeautifulSoup(html, 'html.parser')
+            rating = soup.find('span', class_="num").get_text()
+            brewery2_df.loc[index, 'rating'] = rating.strip("(").strip(")")
+
+        except (AttributeError):
+            print('No Values')
+
+    craft_info = brewery2_df.to_dict('records')
+    #print(craft_info)
+
+
+    return(craft_info)
 
 
